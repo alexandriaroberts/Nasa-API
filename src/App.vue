82c6263bbbb37f37476620b069882c3cdc69984e
Lexie <sb.alexandria@gmail.com>
2019-06-04 21:53:29 +0700

    First draft

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ScrollUi/>
+  </div>
+</template>
+
+<script>
+import ScrollUi from "./components/ScrollUi.vue";
+
+export default {
+  name: "app",
+  components: {
+    ScrollUi
+  }
+};
+</script>
+
+<style>
+html {
+  background: #f0cd58; 
+  overflow-x: hidden;
+  }
+
+
+</style>
